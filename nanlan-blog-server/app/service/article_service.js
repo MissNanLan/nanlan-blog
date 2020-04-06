a4209a6fc62fc6f50c1f0d968aad4db83762e336
jiangyunju <angela.jiang@chinapex.com>
2020-04-06 19:21:03 +0800

    修改分页

--- nanlan-blog-server/app/service/article_service.js
+++ nanlan-blog-server/app/service/article_service.js
@@ -1,45 +1,74 @@
 const articleDao = require("../models/article");
-
 async function articleService(params) {
   return new Promise((resolve, reject) => {
     const reg = new RegExp(params.keyword, "i");
-    var query = articleDao.find({ title: reg });
-    query
-      .skip((params.pageNumber - 1) * params.pageSize)
-      .limit(params.pageSize);
-    query.exec(function(err, res) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
-      }
+
+    var query = articleDao;
+    query.estimatedDocumentCount({ title: reg }, function (err, total) {
+      console.log(total);
+      query
+        .find({ title: reg })
+        .skip((params.pageNumber - 1) * params.pageSize)
+        .limit(params.pageSize)
+        .exec(function (err, res) {
+          if (err) {
+            reject(err);
+          } else {
+            var res = {
+              result: res,
+              amount: total, // 总条数
+              totalPage: parseInt(total / params.pageSize), // 总页数
+            };
+            resolve(res);
+          }
+        });
     });
+
+    // var query = articleDao.find({ title: reg });
+    // query
+    //   .skip((params.pageNumber - 1) * params.pageSize)
+    //   .limit(params.pageSize);
+    // query.exec(function(err, res) {
+    //   if (err) {
+    //     reject(err);
+    //   } else {
+    //     console.log(query.count());
+    //     var res = {
+    //       result: res,
+    //       amount: 10, // 总条数
+    //       totalPage: 2, // 总页数
+    //     }
+    //     resolve(res);
+    //   }
+    // });
   });
 }
 
 async function detailService(params) {
-  return  await articleDao.findOne({
-    _id: params.id
-  }).lean()
+  return await articleDao
+    .findOne({
+      _id: params.id,
+    })
+    .lean();
 }
 
 async function insertArticleService(params) {
   await new articleDao({
     ...params,
-    user_id: "", 
-    like_count: 0, 
-    comment_count: 0, 
-    view_count: 0, 
-  }).save('', (err, res) => { 
+    user_id: "",
+    like_count: 0,
+    comment_count: 0,
+    view_count: 0,
+  }).save("", (err, res) => {
     if (err) {
-    } else { 
-      return "新增成功"
+    } else {
+      return "新增成功";
     }
-  })
+  });
 }
 
 module.exports = {
   articleService,
   insertArticleService,
-  detailService
+  detailService,
 };
