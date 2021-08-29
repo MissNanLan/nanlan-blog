1993ee6cc339a699fcd0f705097ec003da89d142
-g <nanlan.yj@foxmail.com>
2021-08-29 15:23:01 +0800

    del server

--- nanlan-blog-server/app/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const loginSchema = new mongoose.Schema({
-    name: String,
-    pwd: String
-},{
-    collection: 'user'
-})
-
-module.exports = mongoose.model('user',loginSchema)
